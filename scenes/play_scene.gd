7b10250165fa311b68d289f54e1a460a8eadb9f1
Juan Herrera <juanalbertoherrera99@gmail.com>
2024-02-13 17:37:57 -0300

    commit

---
+++ scenes/play_scene.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+const MOTION_SPEED = 300 # Pixels/second.
+
+func _physics_process(_delta):
+	var motion = Vector2()
+	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	motion.y /= 2
+	motion = motion.normalized() * MOTION_SPEED
+	#warning-ignore:return_value_discarded
+	move_and_slide(motion)
+	
+	
+	
+	
+	

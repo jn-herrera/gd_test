94aa6e2ae8f62cce859af6cafa1b1e5dd4c828ed
Juan Herrera <juanalbertoherrera99@gmail.com>
2024-02-14 22:07:56 -0300

    commit 14/02
    Agregado de auto con colision.
    Arreglo de carpetas

--- scenes/ui_interface.gd
+++ scenes/ui_interface.gd
@@ -2,13 +2,17 @@ extends Control
 
 
 # Cargo la escena Menu2 y la almaceno en la variable Menu2
-const Menu2 := preload("res://scenes/option1.tscn")
+const option1 := preload("res://scenes/option1.tscn")
+const scene_playgame := "res://levels/rooms/street_part1.tscn"
+
 
 # Declaración de nodos y variables
 #onready var tween := $Tween
-onready var screens_container := $VBoxContainer
-onready var btn_play := $VBoxContainer/btn_play
-onready var close_button = $VBoxContainer/btn_salir
+
+onready var btn_playgame := $HBoxContainer/btn_play
+onready var screens_container := $HBoxContainer
+onready var btn_opt1 := $HBoxContainer/btn_opt1
+onready var btn_close = $HBoxContainer/btn_salir
 
 # Método llamado cuando el nodo ha sido inicializado
 func _ready() -> void:
@@ -17,8 +21,9 @@ func _ready() -> void:
 	
 	# Conecta la señal "pressed" del botón redirigir
 	# al método "mostrar" con la escena Menu2 como argumento
-	btn_play.connect("pressed", self, "mostrar",[Menu2])
-	close_button.connect("pressed", self, "cerrar_escena")
+	btn_playgame.connect("pressed", self, "mostrar_escena_playgame")
+	btn_opt1.connect("pressed", self, "mostrar",[option1])
+	btn_close.connect("pressed", self, "cerrar_escena")
 
 
 # Método para mostrar una nueva escena
@@ -33,7 +38,6 @@ func mostrar(scene: PackedScene):
 	
 	# Oculta el botón redirigir
 	#btn_play.hide()
-	
 
 	
 # Método para volver a la escena anterior
@@ -48,7 +52,13 @@ func volver() -> void:
 	for child in screens_container.get_children():
 		child.queue_free()
 	
-
+func mostrar_escena_playgame() -> void:
+	# Obtén el SceneTree y cambia a la escena deseada
+	var tree = get_tree()
+	tree.change_scene(scene_playgame)
+	
+	
 func cerrar_escena():
 	get_tree().quit()
-
+	
+	
